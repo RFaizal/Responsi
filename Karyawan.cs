862e81c67e185fd17e33c0e590ba0265f69e458a
Iz <101628793+RFaizal@users.noreply.github.com>
2022-04-19 16:28:22 +0700

    Add files via upload

---
+++ Karyawan.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4317
+{
+    class Karyawan
+    {
+        private string nik;
+        private string nama;
+        private int gajiBulanan;
+
+        public string Nik { get; set; }
+        public string Nama { get; set; }
+        public int GajiBulanan { get; set; }
+       
+
+        public Karyawan(string nik, string nama, int gajiBulanan)
+        {
+            this.nik = nik;
+            this.nama = nama;
+            this.gajiBulanan = gajiBulanan < 0 ? 0 : gajiBulanan;
+        }
+
+        public void Data()
+        {
+            Console.WriteLine("{0} {1}      {2}", nik, nama, gajiBulanan);
+        }
+
+        public void naikGaji()
+        {
+            int kenaikan;
+
+            kenaikan = gajiBulanan * 110;
+            gajiBulanan = kenaikan / 100;
+
+            Console.WriteLine("asyiiik naik gaji 10%");
+        }
+    }
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4317
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan Paijo = new Karyawan("190302123", "Paijo", 4000000);
+            Karyawan Jono = new Karyawan("190302124", "Jono",  2000000);
+
+
+            Console.WriteLine("No nik / nama        Gaji bulanan");
+            Console.WriteLine("---------------------------------");
+            Console.Write("1 ");Paijo.Data();
+            Console.Write("2 ");Jono.Data();
+            Console.WriteLine();
+
+            Paijo.naikGaji();
+            Jono.naikGaji();
+
+            Console.WriteLine();
+            Console.WriteLine("No nik/nama          Gaji bulanan");
+            Console.WriteLine("---------------------------------");
+            Console.Write("1 ");Paijo.Data();
+            Console.Write("2 ");Jono.Data();
+
+            Console.ReadKey();
+
+            
+
+        }
+    }
+}
